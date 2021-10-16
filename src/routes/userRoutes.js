d6a20e1dfedd8d9e30bb18e3a056dde8eebee053
ViniBilk <83610173+ViniBilk@users.noreply.github.com>
2021-10-16 17:15:52 -0300

    Conflict Updates

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -9,11 +9,11 @@ const authentication = require("../middlewares/authentication");
 const userValidations = require("../validations/userValidations");
 
 router.post("/create", usuarioValidations.post, usersControllers.create);
-router.get("/", authentication(["guest", "admin", "user"]), userValidations.get,usersControllers.getUser)
+router.get("/", authentication(["guest", "admin", "user"]), usersControllers.getUser)
 router.put("/edit", authentication(["guest", "admin", "user"]), usuarioValidations.put, usersControllers.editUser)
 router.delete("/delete", authentication(["guest", "admin", "user"]),usuarioValidations.delete , usersControllers.deleteUser)
 router.post("/reserve", authentication(["guest", "admin", "user"]),usuarioValidations.postReserva ,usersControllers.reserve)
 router.delete("/reserve/delete", authentication(["guest", "admin", "user"]), usuarioValidations.deleteReserva, usersControllers.deleteReserva)
-router.get("/reserve/get", authentication(["guest", "admin", "user"]),userValidations.getReserva, usersControllers.getReserva)
+router.get("/reserve/get", authentication(["guest", "admin", "user"]), usersControllers.getReserva)
 
 module.exports = router;
